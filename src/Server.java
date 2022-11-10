34e7556211b9b2f78fde3ac1e3f9ffb232f1c985
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-10 12:30:38 +0300

    create a server socket instance specifying the port on which communication will take place

---
+++ src/Server.java
@@ -0,0 +1,29 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server{
+    //Steps to connecting to a server
+    // 1. The server creates a server Socket specifying the port on which communication will take place in
+    // 2. The server invokes accept() method that waits until a connection is made
+
+    int port = 1234;
+    ServerSocket serverSocket = new ServerSocket(port);
+    //This class is overloaded and takes in more parameters
+    //ServerSocket newSocket = new ServerSocket(int port, int backlog); - A port and the number of devices to be in the queue during connection
+    //  ServerSocket serverSocket2 = new ServerSocket(int port, int backlog, InetAddress address ); - The address to connect to in case the server has many ports
+
+    while(true){
+        try {
+            Socket socket = serverSocket.accept(); // waiting for a device to connect
+
+            DataInputStream inputStream = new DataInputStream(socket.getInputStream());// used to read data from the socket
+            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());// used to write data to the socket
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
